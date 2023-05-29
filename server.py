cf7fa1653eb49194c6c3a60fbea9d0246ccf6dd7
SkandaUdupi <skanda.udupi02@gmail.com>
2023-05-29 22:21:53 +0530

    remove actual answers

--- server.py
+++ server.py
@@ -1,40 +1,30 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return "Hello, welcome to the calculator!"
+    return jsonify("Hello world!")
+
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
     data = request.get_json()
-    num1 = data.get('num1')
-    num2 = data.get('num2')
-    
-    if num1 is None or num2 is None:
-        return jsonify(error="Please provide 'num1' and 'num2' in the request body."), 400
+    first = data.get('first')
+    second = data.get('second')
+    result = first + second
+    return jsonify({"result": result})
 
-    try:
-        result = float(num1) + float(num2)
-        return jsonify(result=result)
-    except ValueError:
-        return jsonify(error="Invalid input. Please provide numeric values for 'num1' and 'num2'."), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     data = request.get_json()
-    num1 = data.get('num1')
-    num2 = data.get('num2')
-    
-    if num1 is None or num2 is None:
-        return jsonify(error="Please provide 'num1' and 'num2' in the request body."), 400
+    first = data.get('first')
+    second = data.get('second')
+    result = first - second
+    return jsonify({"result": result})
 
-    try:
-        result = float(num1) - float(num2)
-        return jsonify(result=result)
-    except ValueError:
-        return jsonify(error="Invalid input. Please provide numeric values for 'num1' and 'num2'."), 400
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0')
