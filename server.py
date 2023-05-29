3112870233e74b7993db15a4d47806f108362fb7
SkandaUdupi <skanda.udupi02@gmail.com>
2023-05-29 22:49:59 +0530

    updated

--- server.py
+++ server.py
@@ -1,30 +1,29 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
+
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return jsonify("Hello world!")
-
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    data = request.get_json()
-    first = data.get('first')
-    second = data.get('second')
-    result = first + second
-    return jsonify({"result": result})
-
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    data = request.get_json()
-    first = data.get('first')
-    second = data.get('second')
-    result = first - second
-    return jsonify({"result": result})
-
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0')
+    app.run(port=8080,host='0.0.0.0')
